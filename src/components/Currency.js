ca80acf4ae11048882175c2b1c73cbe0491c5485
Yhair Tamayo <yhair.tamayo@outlook.com>
2023-06-19 10:54:05 -0500

    Q3 Dropdown currency missing styles

---
+++ src/components/Currency.js
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Currency = () => {
+   const { dispatch, currency } = useContext(AppContext);
+
+   const submitEvent = (currCode) => {
+      dispatch({
+         type: "CHG_CURRENCY",
+         payload: currCode
+      });
+   }
+
+   return (
+      <div className='alert alert-secondary'>
+      <select
+         className="custom-select form-control"
+         id="inputGroupSelect04"
+         onChange={(event) => submitEvent(event.target.value)}
+      >
+         <option value="$" name="dollar">
+         $ Dollar
+         </option>
+         <option value="£" name="pound">
+         £ Pound
+         </option>
+         <option value="€" name="euro">
+         € Euro
+         </option>
+         <option value="₹" name="rupee">
+         ₹ Rupee
+         </option>
+      </select>
+      </div>
+  );
+}
+export default Currency;
