728fbd3437b3f77daedfa8b770b4ac688fb79ce5
Yhair Tamayo <yhair.tamayo@outlook.com>
2023-06-19 10:17:50 -0500

    Q2 Editable Budget OnBlur

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,56 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
-    );
+   const { dispatch, budget } = useContext(AppContext);
+   const [value, setValue] = useState(budget);
+
+   const { expenses } = useContext(AppContext);
+   const totalExpenses = expenses.reduce((total, item) => {
+      return (total += item.cost);
+   }, 0);
+
+   const submitEvent = (value) => {
+      if (value === "" || isNaN(value) || isNaN(parseInt(value))) {
+         alert("Please enter a valid number!");
+         setValue(budget);
+         return;
+      }
+      const budgetValue = parseInt(value);
+      if (budgetValue < totalExpenses) {
+         alert("The budget cannot be less than amount spent £" + totalExpenses);
+         setValue(budget);
+         return;
+      }
+      if (budgetValue > 20000) {
+         alert("The budget cannot be greater than £ 20,000 ");
+         setValue(budget);
+         return;
+      }
+      dispatch({
+         type: "SET_BUDGET",
+         payload: budgetValue,
+      });
+   };
+
+   const handleBlur = () => {
+      submitEvent(value);
+   };
+
+   return (
+      <div className="alert alert-secondary">
+         <span>Budget: £ </span>
+         <input
+            required="required"
+            type="number"
+            step={10}
+            id="budget"
+            value={value}
+            style={{ marginLeft: "0.5rem", size: 10, width: "120px" }}
+            onChange={(event) => setValue(event.target.value)}
+            onBlur={handleBlur}
+         ></input>
+      </div>
+   );
 };
 export default Budget;
